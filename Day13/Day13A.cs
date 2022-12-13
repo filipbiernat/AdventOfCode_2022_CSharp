c7ec11050e3cc85c23b8b89885ab0ddef2643e58
filipbiernat <fbiernat@gmail.com>
2022-12-13 15:08:22 +0100

    Day 13

---
+++ Day13/Day13A.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+
+namespace AdventOfCode2022.Day13
+{
+    public class Day13A : IDay
+    {
+        public enum Result { True, Continue, False };
+
+        public void Run()
+        {
+            // Your handheld device must still not be working properly; the packets from the distress signal got decoded out of order.
+            // You'll need to re-order the list of received packets (your puzzle input) to decode the message.
+            List<string> input = File.ReadAllText(@"..\..\..\Day13\Day13.txt").Split("\r\n\r\n").ToList();
+
+            // Your list consists of pairs of packets; pairs are separated by a blank line.
+            IEnumerable<List<JsonNode?>> pairsOfPackets = input.Select(ParsePairOfPackets);
+
+            // What are the indices of the pairs that are already in the right order?
+            // Determine which pairs of packets are already in the right order. What is the sum of the indices of those pairs?
+            int output = pairsOfPackets
+                .Select((pairOfPackets, index) => IsPairOfPacketsInTheRightOrder(pairOfPackets) ? index + 1 : 0)
+                .Sum();
+
+            Console.WriteLine("Solution: {0}.", output);
+        }
+
+        // Packet data consists of lists and integers. Each list starts with [, ends with ], and contains zero or more comma-separated values
+        // (either integers or other lists). Each packet is always a list and appears on its own line.
+        private static List<JsonNode?> ParsePairOfPackets(string pairOfPackets) => pairOfPackets
+            .Split("\r\n")
+            .Select(list => JsonNode.Parse(list))
+            .ToList();
+
+        private static bool IsPairOfPacketsInTheRightOrder(List<JsonNode?> pairOfPackets)
+        {
+            // When comparing two values, the first value is called left and the second value is called right.
+            JsonNode left = GetNode(pairOfPackets, 0);
+            JsonNode right = GetNode(pairOfPackets, 1);
+            return IsPairOfPacketsInTheRightOrder(left, right) == Result.True;
+        }
+
+        private static Result IsPairOfPacketsInTheRightOrder(JsonNode left, JsonNode right)
+        {
+            // If both values are integers, the lower integer should come first.
+            // If the left integer is lower than the right integer, the inputs are in the right order.
+            if (left is JsonValue && right is JsonValue)
+            {
+                if ((int)left < (int)right) return Result.True;
+                if ((int)left > (int)right) return Result.False;
+                return Result.Continue;
+            }
+
+            // If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
+            JsonArray leftList = left is JsonValue ? new JsonArray((int)left) : (JsonArray)(left);
+            JsonArray rightList = right is JsonValue ? new JsonArray((int)right) : (JsonArray)(right);
+
+            // If both values are lists, compare the first value of each list, then the second value, and so on.
+            for (int index = 0; ; ++index)
+            {
+                // If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+                if (index >= leftList.Count && index >= rightList.Count) return Result.Continue;
+                // If the left list runs out of items first, the inputs are in the right order.
+                if (index >= leftList.Count) return Result.True;
+                // If the right list runs out of items first, the inputs are not in the right order.
+                if (index >= rightList.Count) return Result.False;
+
+                Result result = IsPairOfPacketsInTheRightOrder(GetNode(leftList, index), GetNode(rightList, index));
+                if (result != Result.Continue) return result;
+            }
+        }
+
+        private static JsonNode GetNode(List<JsonNode?> list, int index) => list[index] ?? throw new NullReferenceException("Unexpected null JsonNode");
+        private static JsonNode GetNode(JsonArray list, int index) => list[index] ?? throw new NullReferenceException("Unexpected null JsonNode");
+    }
+}
---
+++ Day13/Day13B.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Nodes;
+
+namespace AdventOfCode2022.Day13
+{
+    public class Day13B : IDay
+    {
+        public void Run()
+        {
+            // Your handheld device must still not be working properly; the packets from the distress signal got decoded out of order.
+            // You'll need to re-order the list of received packets (your puzzle input) to decode the message.
+            List<string> input = File.ReadAllLines(@"..\..\..\Day13\Day13.txt").ToList();
+
+            // Disregard the blank lines in your list of received packets.
+            input.RemoveAll(entry => string.IsNullOrEmpty(entry));
+
+            // The distress signal protocol also requires that you include two additional divider packets: [[2]], [[6]].
+            List<JsonNode> dividerPackets = ParsePackets(new List<string> { "[[2]]", "[[6]]" });
+            List<JsonNode> packets = ParsePackets(input)
+                .Concat(dividerPackets)
+                .ToList();
+
+            // Organize all of the packets into the correct order.
+            packets.Sort(ComparePackets);
+
+            // Afterward, locate the divider packets.
+            // To find the decoder key for this distress signal, you need to determine the indices of the two divider packets and multiply them together. 
+            // What is the decoder key for the distress signal?
+            int output = packets
+                .Select((packet, index) => dividerPackets.Contains(packet) ? index + 1 : 0)
+                .Where(index => index != 0)
+                .Aggregate((lhs, rhs) => lhs * rhs);
+
+            Console.WriteLine("Solution: {0}.", output);
+        }
+
+        // Packet data consists of lists and integers. Each list starts with [, ends with ], and contains zero or more comma-separated values
+        // (either integers or other lists). Each packet is always a list and appears on its own line.
+        private static List<JsonNode> ParsePackets(List<string> packets) => packets
+            .Select(packet => JsonNode.Parse(packet) ?? 0)
+            .ToList();
+
+        private static int ComparePackets(JsonNode left, JsonNode right)
+        {
+            // If both values are integers, the lower integer should come first.
+            // If the left integer is lower than the right integer, the inputs are in the right order.
+            if (left is JsonValue && right is JsonValue)
+            {
+                if ((int)left < (int)right) return -1;
+                if ((int)left > (int)right) return 1;
+                return 0;
+            }
+
+            // If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
+            JsonArray leftList = left is JsonValue ? new JsonArray((int)left) : (JsonArray)(left);
+            JsonArray rightList = right is JsonValue ? new JsonArray((int)right) : (JsonArray)(right);
+
+            // If both values are lists, compare the first value of each list, then the second value, and so on.
+            for (int index = 0; ; ++index)
+            {
+                // If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+                if (index >= leftList.Count && index >= rightList.Count) return 0;
+                // If the left list runs out of items first, the inputs are in the right order.
+                if (index >= leftList.Count) return -1;
+                // If the right list runs out of items first, the inputs are not in the right order.
+                if (index >= rightList.Count) return 1;
+
+                int result = ComparePackets(GetNode(leftList, index), GetNode(rightList, index));
+                if (result != 0) return result;
+            }
+        }
+
+        private static JsonNode GetNode(JsonArray list, int index) => list[index] ?? throw new NullReferenceException("Unexpected null JsonNode");
+    }
+}
